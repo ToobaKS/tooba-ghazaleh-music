d651432292f77fb522739638c7e155000e383cca
Ghazaleh Ahmadzadeh <ghazalehahmadzadeh@ymail.com>
2025-02-27 17:44:36 -0500

    Final Commit

--- src/components/SongForm/SongForm.jsx
+++
@@ -1,7 +0,0 @@
-function SongForm() {
-    return (
-      <h3>Song form</h3>
-    );
-  }
-  
-  export default SongForm;
--- src/components/SongLyrics/SongLyrics.jsx
+++
@@ -1,7 +0,0 @@
-function SongLyric() {
-    return (
-      <h3>Song Lyric</h3>
-    );
-  }
-  
-  export default SongLyric;
